be1014c4e7ac831752f9ae31b55e7e2b22d38145
Anatoly Filyuk <buherpet.38@gmail.com>
2022-10-07 16:08:53 +0500

    .

---
+++ ProjectManagement/MyContext.cs
@@ -0,0 +1,32 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Pages.Projects;
+
+namespace ProjectManagement
+{
+    public class MyContext : DbContext
+    {
+        public MyContext()
+        {
+
+        }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ ProjectManagement/Pages/Projects/Project.cs
@@ -0,0 +1,52 @@
+﻿using RazorClassLibrary;
+
+namespace ProjectManagement.Pages.Projects
+{
+    public class ProjectView : BaseView<Project, ProjectFactory, MyContext>
+    {
+        [Field(DisplayName = "#")]
+        public int Id { get; set; }
+
+        [Field(DisplayName = "Наименование")]
+        public string? Name { get; set; }
+
+        public override string GetName() => "Проект";
+
+        public override string GetNames() => "Проекты";
+
+        public override string GetEntityName() => "project";
+
+        public override string GetEntityNames() => "projects";
+    }
+
+    public class ProjectAllView : ProjectView
+    {
+        public override IQueryable GetData(string? filter)
+        {
+            var clients = new MyContext().Projects.AsQueryable();
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                clients = clients.Where(x => x.Name.ToLower().Contains(filter.ToLower()));
+            }
+
+            return clients.OrderByDescending(x => x.Id);
+        }
+    }
+
+    public class Project : IEntity
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string Url => $"clients/{Id}";
+
+        public override string ToString() => $"{Name}";
+    }
+
+    public class ProjectFactory : BaseFactory<Project, MyContext>
+    {
+
+    }
+}
